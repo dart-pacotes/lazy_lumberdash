10e79a90828d7fd5533bc61669aa04957d3977ea
freitzzz <1160907@isep.ipp.pt>
2020-10-16 23:22:19 +0100

    Added periodic and stack limit implementation of lazy lumberdash

--- lib/lazy_lumberdash.dart
+++ lib/lazy_lumberdash.dart
@@ -1,1 +1,4 @@
 library lazy_lumberdash;
+
+export 'package:lumberdash/lumberdash.dart';
+export 'src/lazy_lumberdash.dart';
---
+++ lib/src/lazy_lumberdash.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+
+import 'package:lumberdash/lumberdash.dart';
+import 'package:meta/meta.dart';
+
+abstract class LazyLumberdash extends LumberdashClient {
+  final LumberdashClient _innerLumberdashClient;
+
+  final List<Function> _logCalls = [];
+
+  bool _lock = false;
+
+  LazyLumberdash({
+    @required final LumberdashClient client,
+  }) : _innerLumberdashClient = client;
+
+  @override
+  void logError(dynamic exception, [dynamic stacktrace]) {
+    return registerLogCall(
+      () => _innerLumberdashClient.logError(exception, stacktrace),
+    );
+  }
+
+  @override
+  void logFatal(String message, [Map<String, String> extras]) {
+    return registerLogCall(
+      () => _innerLumberdashClient.logFatal(message, extras),
+    );
+  }
+
+  @override
+  void logMessage(String message, [Map<String, String> extras]) {
+    return registerLogCall(
+      () => _innerLumberdashClient.logMessage(message, extras),
+    );
+  }
+
+  @override
+  void logWarning(String message, [Map<String, String> extras]) {
+    return registerLogCall(
+      () => _innerLumberdashClient.logWarning(message, extras),
+    );
+  }
+
+  @mustCallSuper
+  void registerLogCall(final void Function() call) {
+    _logCalls.add(call);
+
+    onNewLogCall(call);
+  }
+
+  @mustCallSuper
+  void dispatchLogCalls() {
+    if (!_lock) {
+      _lock = true;
+      for (var i = 0; i < _logCalls.length; i++) {
+        _logCalls.removeAt(i)();
+      }
+
+      _lock = false;
+    }
+  }
+
+  void onNewLogCall(final void Function() call);
+}
+
+class PeriodicLazyLumberdash extends LazyLumberdash {
+  final Duration _innerDuration;
+
+  Timer _periodicTimer;
+
+  PeriodicLazyLumberdash({
+    @required final Duration duration,
+  }) : _innerDuration = duration;
+
+  @override
+  void onNewLogCall(final void Function() call) {
+    if (_periodicTimer == null) {
+      Timer.periodic(
+        _innerDuration,
+        (_) {
+          super.dispatchLogCalls();
+        },
+      );
+    }
+  }
+}
+
+class StackLazyLumberdash extends LazyLumberdash {
+  final int limit;
+
+  StackLazyLumberdash({
+    @required final this.limit,
+  });
+
+  @override
+  void onNewLogCall(final void Function() call) {
+    if (super._logCalls.length >= limit) {
+      super.dispatchLogCalls();
+    }
+  }
+}

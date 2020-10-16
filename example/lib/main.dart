c0c0dbdc8dffb8ebc1664d413830594912dde102
freitzzz <1160907@isep.ipp.pt>
2020-10-17 00:09:04 +0100

    Added example for periodic lazy lumberdash

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:lazy_lumberdash/lazy_lumberdash.dart';
 import 'package:print_lumberdash/print_lumberdash.dart';
 
-void main() {
+void main() async {
+  // Using StackLazyLumberdash
+
+  final stackLazyLumberdash = StackLazyLumberdash(
+    limit: 5,
+    client: PrintLumberdash(),
+  );
+
   putLumberdashToWork(
-    withClients: [
-      StackLazyLumberdash(
-        limit: 5,
-        client: PrintLumberdash(),
-      ),
-    ],
+    withClients: [stackLazyLumberdash],
   );
 
   logMessage('#1 - This message should not trigger lazy lumberdash');
@@ -30,4 +32,25 @@ void main() {
   logMessage('#9 - This message must trigger lazy lumberdash');
 
   print('#10 - This message must be logged after #9');
+
+  // Using PeriodicLazyLumberdash
+
+  final periodicLazyLumberdash = PeriodicLazyLumberdash(
+    duration: const Duration(seconds: 3),
+    client: PrintLumberdash(),
+  );
+
+  putLumberdashToWork(
+    withClients: [periodicLazyLumberdash],
+  );
+
+  logMessage('#1 - This message should not trigger lazy lumberdash');
+
+  print('#2 - This message must be logged before #1');
+
+  await Future.delayed(const Duration(seconds: 3));
+
+  print('#3 - This message must be logged after #1');
+
+  periodicLazyLumberdash.close();
 }
